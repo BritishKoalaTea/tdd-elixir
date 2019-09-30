8df1bb89c9eac506974d7cefce8f288f65d354c8
Alison Hawke <44695181+BritishKoalaTea@users.noreply.github.com>
2019-09-30 14:47:30 -0500

    Update versions of deps and language

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tddelixir.MixProject do
     [
       app: :tddelixir,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -26,7 +26,7 @@ defmodule Tddelixir.MixProject do
 	  {:httpotion, "~> 3.0.2"}, 
       {:poison, "~> 3.0"}, 
       {:sweet_xml, "~> 0.6.5"},
-      {:timex, "~> 3.0"}
+      {:timex, "~> 3.6"}
     ]
   end
 end
